ef7ddac86402aed100ffa945a1291ac8e6b78fc4
MAbdelshafyy <167443696+MAbdelshafyy@users.noreply.github.com>
2024-04-18 18:22:53 +0200

    Update DEFS.h

--- DEFS.h
+++ DEFS.h
@@ -7,12 +7,35 @@
 enum ActionType //The actions supported (you can add more if needed)
 {
 	DRAW_RECT,		//Draw Rectangle
+//marwan
+DRAW_SQR,
+DRAW_TRI,
+DRAW_HEX,
+DRAW_CIRC,
+DO_SLCT,
+DO_BRDR,
+DO_FILL,
+DO_DLT,
+DO_DLTALL,
+DO_CPY,
+DO_CUT,
+DO_PST,
+DO_BTOF,
+DO_SAVE,
+DO_LOAD,
+TO_PLAY,		//Switch interface to Play mode
+//
 	EXIT,			//Exit
+//MARWAN
+PCKCLR,
+PCKSHP,
+PCKBTH,
+//
 	TO_DRAW,		//Switch interface to Draw mode
-	TO_PLAY,		//Switch interface to Play mode
 	EMPTY,			//A click on empty place in the toolbar
 	DRAWING_AREA,	//A click on the drawing area
 	STATUS			//A click on the status bar
+
 
 	///TODO: Add the other action types of the two modes
 };
